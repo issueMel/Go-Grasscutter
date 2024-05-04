0b7cfbf9ae3c0ff9e7dd2e2656a6e7b8e18d6498
issueMel <2291381998@qq.com>
2024-05-04 15:33:57 +0800

    update crypto

--- src/utils/crypto/crypto.go
+++ src/utils/crypto/crypto.go
@@ -1,25 +1,36 @@
 package crypto
 
 import (
+	"Go-Grasscutter/src/server/http/object"
 	"Go-Grasscutter/src/utils"
+	"Go-Grasscutter/src/utils/algorithms/mt19937"
+	"crypto"
 	"crypto/rand"
 	"crypto/rsa"
+	"crypto/sha256"
 	"crypto/x509"
+	"encoding/base64"
+	"encoding/binary"
+	"errors"
+	"fmt"
 	"log"
+	"math/big"
 	"regexp"
 	"strconv"
 )
 
 var (
 	DispatchKey       []byte
-	DispatchSeed      []byte
+	DispatchSeed      []byte // todo check if race in regionhandler.go:74
 	EncryptKey        []byte
+	EncryptSeed       = int64(-6978694759076345648) // todo check 11468049314633205968/-6978694759076345648/9223372036854775807
 	EncryptSeedBuffer []byte
 	CurSigningKey     *rsa.PrivateKey
 	EncryptionKeys    = make(map[int]*rsa.PublicKey)
 )
 
 func LoadKeys() {
+
 	DispatchKey = utils.ReadResource("/keys/dispatchKey.bin")
 	DispatchSeed = utils.ReadResource("/keys/dispatchSeed.bin")
 	EncryptKey = utils.ReadResource("/keys/secretKey.bin")
@@ -86,4 +97,92 @@ func CreateSessionKey(length int) []byte {
 		log.Println(err)
 	}
 	return bytes
+}
+
+func Xor(packet, key []byte) []byte {
+	for i := 0; i < len(packet); i++ {
+		packet[i] ^= key[i%len(key)]
+	}
+	return packet
+}
+
+func toInt(s string) int {
+	i, _ := new(big.Int).SetString(s, 10)
+	return int(i.Int64())
+}
+
+func EncryptAndSignRegionData(regionInfo []byte, keyID string) (*object.QueryCurRegionRspJson, error) {
+	if keyID == "" {
+		return nil, errors.New("Key ID was not set")
+	}
+
+	// Get encryption key
+	val, err := strconv.Atoi(keyID)
+	if err != nil {
+		return nil, err
+	}
+	encryptionKey := EncryptionKeys[val]
+	if encryptionKey == nil {
+		return nil, errors.New("Encryption key not found")
+	}
+
+	// Encrypt regionInfo in chunks
+	var encryptedRegionInfo []byte
+	chunkSize := 256 - 11
+	regionInfoLength := len(regionInfo)
+	// math.Ceil()
+	numChunks := (regionInfoLength + chunkSize - 1) / chunkSize
+	for i := 0; i < numChunks; i++ {
+		start := i * chunkSize
+		end := (i + 1) * chunkSize
+		if end > regionInfoLength {
+			end = regionInfoLength
+		}
+		chunk := regionInfo[start:end]
+
+		encryptedChunk, err := rsa.EncryptPKCS1v15(rand.Reader, encryptionKey, chunk)
+		if err != nil {
+			return nil, fmt.Errorf("encryption error: %v", err)
+		}
+		encryptedRegionInfo = append(encryptedRegionInfo, encryptedChunk...)
+	}
+
+	// Sign regionInfo
+	hashed := sha256.Sum256(regionInfo)
+	signature, err := rsa.SignPKCS1v15(rand.Reader, CurSigningKey, crypto.SHA256, hashed[:])
+	if err != nil {
+		return nil, fmt.Errorf("signing error: %v", err)
+	}
+
+	// Base64 encode encrypted data and signature
+	encodedContent := base64.StdEncoding.EncodeToString(encryptedRegionInfo)
+	encodedSign := base64.StdEncoding.EncodeToString(signature)
+
+	return &object.QueryCurRegionRspJson{
+		Content: encodedContent,
+		Sign:    encodedSign,
+	}, nil
+}
+
+func GenerateEncryptKeyAndSeed(encryptKey []byte) int64 {
+	var encryptSeed int64
+	binary.Read(rand.Reader, binary.LittleEndian, &encryptSeed)
+
+	mt := mt19937.New()
+	mt.Seed(encryptSeed)
+	mt.Seed(int64(mt.NextUint64()))
+	mt.NextUint64()
+
+	for i := 0; i < 4096>>3; i++ {
+		rand := mt.NextUint64()
+		encryptKey[i<<3] = byte(rand >> 56)
+		encryptKey[(i<<3)+1] = byte(rand >> 48)
+		encryptKey[(i<<3)+2] = byte(rand >> 40)
+		encryptKey[(i<<3)+3] = byte(rand >> 32)
+		encryptKey[(i<<3)+4] = byte(rand >> 24)
+		encryptKey[(i<<3)+5] = byte(rand >> 16)
+		encryptKey[(i<<3)+6] = byte(rand >> 8)
+		encryptKey[(i<<3)+7] = byte(rand)
+	}
+	return encryptSeed
 }
--- src/utils/utils.go
+++ src/utils/utils.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"encoding/base64"
 	"github.com/cloudwego/hertz/pkg/app"
+	"log"
 )
 
 var (
@@ -42,4 +43,23 @@ func BytesToHex(bytes []byte) string {
 
 func Base64Encode(toEncode []byte) string {
 	return base64.StdEncoding.EncodeToString(toEncode)
+}
+
+func Base64Decode(toDecode string) []byte {
+	decodedData, err := base64.StdEncoding.DecodeString(toDecode)
+	if err != nil {
+		log.Println(err)
+	}
+	return decodedData
+}
+
+func AbilityHash(str string) int {
+	bytes := []rune(str)
+	v7 := 0
+	v8 := 0
+	for v8 < len(str) {
+		v7 = int(bytes[v8]) + 131*v7
+		v8++
+	}
+	return v7
 }
