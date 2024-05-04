bdd7e06c5e66ebae0c5e9c189735f12b920e79bb
issueMel <2291381998@qq.com>
2024-05-04 15:32:38 +0800

    Add log translate file support Functions

--- src/config/init.go
+++ src/config/init.go
@@ -1,21 +1,30 @@
 package config
 
+import "C"
 import (
+	"Go-Grasscutter/src/utils"
+	"bytes"
 	_ "embed"
+	"fmt"
 	"github.com/spf13/viper"
+	"log"
 	"os"
 )
 
 //go:embed config.json
 var config []byte
 
-var C *Config
+var (
+	C *Config
+	L *viper.Viper
+)
 
 func InitConfig() {
 	filepath := "config.json"
 	_, err := os.Stat(filepath)
 	// If no configuration in the directory.
 	// Generate the configuration.
+	// todo Generate without config.json
 	if err != nil {
 		if os.IsNotExist(err) {
 			err := os.WriteFile(filepath, config, 0644)
@@ -33,6 +42,21 @@ func InitConfig() {
 	}
 	err = viper.Unmarshal(&C)
 	if err != nil {
+		panic(err)
+	}
+	initLanguage()
+}
+
+func initLanguage() {
+	L = viper.New()
+	L.AddConfigPath("./")
+	r, err := utils.GetResource().ReadFile(fmt.Sprintf("resources/languages/%s.json", C.Language.Language))
+	if err != nil {
+		log.Println(err)
+		r, _ = utils.GetResource().ReadFile("resources/languages/en-US.json")
+	}
+	if err = L.ReadConfig(bytes.NewBuffer(r)); err != nil {
+		// en-US.json should exist
 		panic(err)
 	}
 }
