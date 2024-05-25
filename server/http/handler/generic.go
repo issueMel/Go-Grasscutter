9b1df845ff87a23709d59fe3a2947585c210d451
issueMel <2291381998@qq.com>
2024-05-25 09:54:56 +0800

    update generic

--- server/http/handler/generic.go
+++ server/http/handler/generic.go
@@ -18,13 +18,17 @@ func ApplyGenericHandler(r *server.Hertz) {
 	r.POST("/account/risky/api/check", Response(
 		"{\"retcode\":0,\"message\":\"OK\",\"data\":{\"id\":\"none\",\"action\":\"ACTION_NONE\",\"geetest\":null}}"))
 
+	// sdk-os-static.hoyoverse.com
+	r.GET("/combo/box/api/config/sdk/combo", Response(
+		"{\"retcode\":0,\"message\":\"OK\",\"data\":{\"vals\":{\"disable_email_bind_skip\":\"false\",\"email_bind_remind_interval\":\"7\",\"email_bind_remind\":\"true\"}}}"))
+
+	// hk4e-sdk-os-static.hoyoverse.com
+	r.GET("/hk4e_global/mdk/shield/api/getConfig", Response(
+		"{\"retcode\":0,\"message\":\"OK\",\"data\":{\"protocol\":true,\"qr_enabled\":false,\"log_level\":\"INFO\",\"announce_url\":\"https://webstatic-sea.hoyoverse.com/hk4e/announcement/index.html?sdk_presentation_style=fullscreen\\u0026sdk_screen_transparent=true\\u0026game_biz=hk4e_global\\u0026auth_appid=announcement\\u0026game=hk4e#/\",\"push_alias_type\":2,\"disable_ysdk_guard\":false,\"enable_announce_pic_popup\":true}}"))
+
 	// hk4e-sdk-os-static.hoyoverse.com
 	r.GET("/hk4e_global/mdk/shield/api/loadConfig", Response(
 		"{\"retcode\":0,\"message\":\"OK\",\"data\":{\"id\":6,\"game_key\":\"hk4e_global\",\"client\":\"PC\",\"identity\":\"I_IDENTITY\",\"guest\":false,\"ignore_versions\":\"\",\"scene\":\"S_NORMAL\",\"name\":\"原神海外\",\"disable_regist\":false,\"enable_email_captcha\":false,\"thirdparty\":[\"fb\",\"tw\"],\"disable_mmt\":false,\"server_guest\":false,\"thirdparty_ignore\":{\"tw\":\"\",\"fb\":\"\"},\"enable_ps_bind_account\":false,\"thirdparty_login_configs\":{\"tw\":{\"token_type\":\"TK_GAME_TOKEN\",\"game_token_expires_in\":604800},\"fb\":{\"token_type\":\"TK_GAME_TOKEN\",\"game_token_expires_in\":604800}}}}"))
-
-	// hk4e-sdk-os-static.hoyoverse.com
-	r.GET("/combo/box/api/config/sdk/combo", Response(
-		"{\"retcode\":0,\"message\":\"OK\",\"data\":{\"vals\":{\"disable_email_bind_skip\":\"false\",\"email_bind_remind_interval\":\"7\",\"email_bind_remind\":\"true\"}}}"))
 
 	// Upload device info
 	// abtest-api-data-sg.hoyoverse.com
